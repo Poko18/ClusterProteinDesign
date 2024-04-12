3e0eb89c5d4544307daf25de4c4d9199d1daf1a8
Poko18 <tadej.satler@gmail.com>
2024-04-12 21:37:59 +0200

    update script

--- pipelines/binder_design/helper_scripts/binder_dock.sh
+++ pipelines/binder_design/helper_scripts/binder_dock.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 
-
+set -e
 # Load Anaconda environment
 source /home/tsatler/anaconda3/etc/profile.d/conda.sh
 
@@ -62,6 +62,7 @@ rf_out=$output/rf_dock/${prefix}_${subfolder_name}_${SLURM_ARRAY_TASK_ID}/${pref
 conda activate SE3nv
 
 python /home/tsatler/RFdif/RFdiffusion/scripts/run_inference.py \
+diffuser.T=50 \
 scaffoldguided.target_path=$target_pdb \
 inference.output_prefix=${rf_out} \
 scaffoldguided.scaffoldguided=True \
@@ -70,18 +71,20 @@ scaffoldguided.target_pdb=True \
 scaffoldguided.target_ss=$ss \
 scaffoldguided.target_adj=$adj \
 scaffoldguided.scaffold_dir=$scaff_dir \
-'potentials.guiding_potentials=["type:binder_distance_ReLU","type:binder_ROG","type:interface_ncontacts"]' \
+'potentials.guiding_potentials=["type:binder_ROG,weight:3","type:interface_ncontacts","type:binder_distance_ReLU"]' \
 potentials.guide_scale=2 \
 potentials.guide_decay="quadratic" \
 inference.num_designs=$num_of_diffusions \
 denoiser.noise_scale_ca=0 \
 denoiser.noise_scale_frame=0
 
+#"type:binder_distance_ReLU",
+
 # Remove some files
 current_dir=$(pwd)
-if [ -d "$current_dir/outputs" ]; then
-    rm -r "$current_dir/outputs" # remove hydra stuff
-fi
+#if [ -d "$current_dir/outputs" ]; then
+#    rm -r "$current_dir/outputs" # remove hydra stuff
+#fi
 if [ -e "$rf_out*.trb" ]; then
     rm "$rf_out*.trb" # delete trb files
 fi
@@ -108,13 +111,22 @@ else
     script="helper_scripts/colabinder.py"
 fi
 
+#for ((i=0; i<${#input_files[@]}; i++)); do
+#  pdb_file=${input_files[$i]}
+#  echo $pdb_file AF2 design
+#  af_out=$output/mpnn_af2/${prefix}_${subfolder_name}_${SLURM_ARRAY_TASK_ID}
+#  python $script $pdb_file $af_out B A --sampling_temp $sampling_temp \
+#  --num_recycles $num_recycles --num_seqs $total_mpnn --num_filt_seq $mpnn_per_design \
+#  --results_dataframe $output --break_design --thread_sequence --save_best_only \
+#  --scaffold_folder $scaff_dir
+#done
+
 for ((i=0; i<${#input_files[@]}; i++)); do
   pdb_file=${input_files[$i]}
   echo $pdb_file AF2 design
   af_out=$output/mpnn_af2/${prefix}_${subfolder_name}_${SLURM_ARRAY_TASK_ID}
   python $script $pdb_file $af_out B A --sampling_temp $sampling_temp \
   --num_recycles $num_recycles --num_seqs $total_mpnn --num_filt_seq $mpnn_per_design \
-  --results_dataframe $output --break_design --thread_sequence --save_best_only \
-  --scaffold_folder $scaff_dir
+  --results_dataframe $output --save_best_only
 done
 

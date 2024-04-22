55b453f6a33bdc493b9700d0a09e9dcf28b57f61
Poko18 <tadej.satler@gmail.com>
2024-04-22 19:39:17 +0200

    final linter

--- pipelines/binder_design/helper_scripts/binder_opt.py
+++ pipelines/binder_design/helper_scripts/binder_opt.py
@@ -1,31 +1,58 @@
-import os,sys
-
-from colabdesign.mpnn import mk_mpnn_model, clear_mem
-from colabdesign.af import mk_af_model
-import pandas as pd
-import numpy as np
 import argparse
 import fcntl
+import os
+import sys
 import time
+
 import matplotlib.pyplot as plt
-#from pyrosetta import *
-#from pyrosetta.rosetta import *
-#from pyrosetta.rosetta.protocols import *
+import numpy as np
+import pandas as pd
+from colabdesign.af import mk_af_model
+from colabdesign.mpnn import clear_mem, mk_mpnn_model
+
+# from pyrosetta import *
+# from pyrosetta.rosetta import *
+# from pyrosetta.rosetta.protocols import *
 
 
-parser = argparse.ArgumentParser(description='Run AFDesign with MPNN sampling')
-parser.add_argument('pdb', type=str, help='input pdb file path')
-parser.add_argument('output_folder', type=str, help='output folder path')
-parser.add_argument('target_chains', type=str, help='target chain IDs (comma-separated)')
-parser.add_argument('binder_chains', type=str, help='binder chain IDs (comma-separated)')
-parser.add_argument('iterations', type=int, default=10, help='number of ProteinMPNN/AF2 iterations')
-parser.add_argument('--initial_proteinmpnns', type=int, default=100, help='initial proteinmpnns (default: 4)')
-parser.add_argument('--designs_per_iteration', type=int, default=10, help='number of designs for further optimization (default: 10)')
-parser.add_argument('--proteinmpnn_per_input', type=int, default=4, help='proteinmpnn_per_input pdb in iteration (default: 4)')
-parser.add_argument('--sort_by', type=str, default="i_pae", help='score to sort during iteration (default: i_pae)')
-parser.add_argument('--num_recycles', type=int, default=12, help='number of af2 cycles (default: 12)')
-parser.add_argument('--sampling_temp', type=float, default=0.15, help='sampling temperature (default: 0.15)')
-parser.add_argument('--mpnn_batch', type=int, default=5, help='proteinMPNN batch (default: 5)')
+parser = argparse.ArgumentParser(description="Run AFDesign with MPNN sampling")
+parser.add_argument("pdb", type=str, help="input pdb file path")
+parser.add_argument("output_folder", type=str, help="output folder path")
+parser.add_argument("target_chains", type=str, help="target chain IDs (comma-separated)")
+parser.add_argument("binder_chains", type=str, help="binder chain IDs (comma-separated)")
+parser.add_argument("iterations", type=int, default=10, help="number of ProteinMPNN/AF2 iterations")
+parser.add_argument(
+    "--initial_proteinmpnns",
+    type=int,
+    default=100,
+    help="initial proteinmpnns (default: 4)",
+)
+parser.add_argument(
+    "--designs_per_iteration",
+    type=int,
+    default=10,
+    help="number of designs for further optimization (default: 10)",
+)
+parser.add_argument(
+    "--proteinmpnn_per_input",
+    type=int,
+    default=4,
+    help="proteinmpnn_per_input pdb in iteration (default: 4)",
+)
+parser.add_argument(
+    "--sort_by",
+    type=str,
+    default="i_pae",
+    help="score to sort during iteration (default: i_pae)",
+)
+parser.add_argument("--num_recycles", type=int, default=12, help="number of af2 cycles (default: 12)")
+parser.add_argument(
+    "--sampling_temp",
+    type=float,
+    default=0.15,
+    help="sampling temperature (default: 0.15)",
+)
+parser.add_argument("--mpnn_batch", type=int, default=5, help="proteinMPNN batch (default: 5)")
 args = parser.parse_args()
 
 # Initialize PyRosetta
@@ -33,35 +60,36 @@ args = parser.parse_args()
 
 pdb = args.pdb
 output_folder = args.output_folder
-target_chains = args.target_chains.split(',')
-binder_chains = args.binder_chains.split(',')
+target_chains = args.target_chains.split(",")
+binder_chains = args.binder_chains.split(",")
 iterations = args.iterations
 
-initial_proteinmpnns=args.initial_proteinmpnns
-designs_per_iteration=args.designs_per_iteration
-proteinmpnn_per_input=args.proteinmpnn_per_input
-sort_by=args.sort_by
+initial_proteinmpnns = args.initial_proteinmpnns
+designs_per_iteration = args.designs_per_iteration
+proteinmpnn_per_input = args.proteinmpnn_per_input
+sort_by = args.sort_by
 
 num_recycles = args.num_recycles
 sampling_temp = args.sampling_temp
 mpnn_batch = args.mpnn_batch
 
-t0=time.time()
+t0 = time.time()
 
 ### Functions ###
-def save_to_csv(df,output_path):
-  if os.path.isfile(output_path):
-    with open(output_path, 'a') as f:
-        fcntl.flock(f, fcntl.LOCK_EX)  # lock the file for exclusive access
-        time.sleep(np.random.uniform(0, 0.05))
-        df.to_csv(f, header=False, index=False)
-        fcntl.flock(f, fcntl.LOCK_UN)  # release the lock
-  else:
-      with open(output_path, 'w') as f:
-          fcntl.flock(f, fcntl.LOCK_EX)  # lock the file for exclusive access
-          time.sleep(np.random.uniform(0, 0.05))
-          df.to_csv(f, index=False)
-          fcntl.flock(f, fcntl.LOCK_UN)  # release the lock
+def save_to_csv(df, output_path):
+    if os.path.isfile(output_path):
+        with open(output_path, "a") as f:
+            fcntl.flock(f, fcntl.LOCK_EX)  # lock the file for exclusive access
+            time.sleep(np.random.uniform(0, 0.05))
+            df.to_csv(f, header=False, index=False)
+            fcntl.flock(f, fcntl.LOCK_UN)  # release the lock
+    else:
+        with open(output_path, "w") as f:
+            fcntl.flock(f, fcntl.LOCK_EX)  # lock the file for exclusive access
+            time.sleep(np.random.uniform(0, 0.05))
+            df.to_csv(f, index=False)
+            fcntl.flock(f, fcntl.LOCK_UN)  # release the lock
+
 
 def create_dataframe(iterations, pdb_output):
     result = []
@@ -91,6 +119,7 @@ def create_dataframe(iterations, pdb_output):
     df = pd.DataFrame(result, columns=labels)
     return df
 
+
 def plot_columns(df, columns, save_path=None):
     # Calculate the number of rows and columns in the grid
     num_plots = len(columns)
@@ -98,19 +127,19 @@ def plot_columns(df, columns, save_path=None):
     num_rows = math.ceil(num_plots / num_cols)
 
     # Create the grid of subplots
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(12,6))
-    
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 6))
+
     # Flatten the axes array for easier indexing
     axes = axes.flatten()
 
     # Plot each specified column against the "iterations" column
     for i, column in enumerate(columns):
         ax = axes[i]  # Get the current subplot
-        ax.plot(df['iterations'], df[column], label=column)
+        ax.plot(df["iterations"], df[column], label=column)
 
         # Add labels and a legend to each subplot
-        ax.set_xlabel('Iterations')
-        ax.set_ylabel('Value')
+        ax.set_xlabel("Iterations")
+        ax.set_ylabel("Value")
         ax.set_title(column)
         ax.legend()
 
@@ -128,169 +157,199 @@ def plot_columns(df, columns, save_path=None):
     else:
         plt.show()
 
+
 #################
 
 print("running initial proteinMPNN")
 
 ### Initialize ###
 # output_folder all output
-pdb_basename=pdb.split("/")[-1].split(".pdb")[0]
-pdb_output=f"{output_folder}/binders/{pdb_basename}"
+pdb_basename = pdb.split("/")[-1].split(".pdb")[0]
+pdb_output = f"{output_folder}/binders/{pdb_basename}"
 os.system(f"mkdir -p {output_folder}")
 os.system(f"mkdir -p {pdb_output}")
 
-af_terms = ["plddt","i_ptm","i_pae","rmsd"]
-other_terms= ["model_path","input_pdb"]
+af_terms = ["plddt", "i_ptm", "i_pae", "rmsd"]
+other_terms = ["model_path", "input_pdb"]
 labels = ["score"] + af_terms + other_terms
 
-af_model = mk_af_model(protocol="binder",
-                       initial_guess = True,
-                       best_metric = "rmsd",
-                       use_multimer = True,
-                       data_dir="/home/tsatler/projects/AFdesign_playground",
-                       model_names = ["model_1_multimer_v3"])
+af_model = mk_af_model(
+    protocol="binder",
+    initial_guess=True,
+    best_metric="rmsd",
+    use_multimer=True,
+    data_dir="/home/tsatler/projects/AFdesign_playground",
+    model_names=["model_1_multimer_v3"],
+)
 mpnn_model = mk_mpnn_model(weights="soluble")
 
 
 # Run initial mpnn/af2
-initial_csv=f"{pdb_output}/it_0.csv"
+initial_csv = f"{pdb_output}/it_0.csv"
 
 if not os.path.exists(initial_csv):
-   af_model.prep_inputs(pdb,target_chain=",".join(target_chains),binder_chain=",".join(binder_chains),rm_aa="C")
+    af_model.prep_inputs(
+        pdb,
+        target_chain=",".join(target_chains),
+        binder_chain=",".join(binder_chains),
+        rm_aa="C",
+    )
 
-   mpnn_model.get_af_inputs(af_model)
-   mpnn_out = mpnn_model.sample(num=initial_proteinmpnns//mpnn_batch, batch=mpnn_batch, temperature=sampling_temp)
-   for k in af_terms: mpnn_out[k] = []
-   for term in other_terms: 
-      if term not in mpnn_out:
-         mpnn_out[term] = []
+    mpnn_model.get_af_inputs(af_model)
+    mpnn_out = mpnn_model.sample(
+        num=initial_proteinmpnns // mpnn_batch,
+        batch=mpnn_batch,
+        temperature=sampling_temp,
+    )
+    for k in af_terms:
+        mpnn_out[k] = []
+    for term in other_terms:
+        if term not in mpnn_out:
+            mpnn_out[term] = []
 
-   # Predict initial sequences
-   for n in range(initial_proteinmpnns):
-      seq = mpnn_out["seq"][n][-af_model._len:]
-      af_model.predict(seq=seq, num_recycles=num_recycles, num_models=1, verbose=False)
-      
-      for t in af_terms: mpnn_out[t].append(af_model.aux["log"][t])
-      if "i_pae" in mpnn_out:
-         mpnn_out["i_pae"][-1] = mpnn_out["i_pae"][-1] * 31
-      if "pae" in mpnn_out:
-         mpnn_out["pae"][-1] = mpnn_out["pae"][-1] * 31
-      
-      iteration=0
-      iteration_folder=f"{pdb_output}/iterations/it_{iteration}"
-      os.system(f"mkdir -p {iteration_folder}")
-      current_model=f"{iteration_folder}/{pdb_basename}_{iteration}_{n}.pdb"
-      mpnn_out["model_path"].append(current_model)
-      mpnn_out["input_pdb"].append(pdb)
+    # Predict initial sequences
+    for n in range(initial_proteinmpnns):
+        seq = mpnn_out["seq"][n][-af_model._len :]
+        af_model.predict(seq=seq, num_recycles=num_recycles, num_models=1, verbose=False)
 
-      af_model.save_current_pdb(f"{current_model}")
-      af_model._save_results(save_best=True, verbose=False)
-      af_model._k += 1
+        for t in af_terms:
+            mpnn_out[t].append(af_model.aux["log"][t])
+        if "i_pae" in mpnn_out:
+            mpnn_out["i_pae"][-1] = mpnn_out["i_pae"][-1] * 31
+        if "pae" in mpnn_out:
+            mpnn_out["pae"][-1] = mpnn_out["pae"][-1] * 31
 
+        iteration = 0
+        iteration_folder = f"{pdb_output}/iterations/it_{iteration}"
+        os.system(f"mkdir -p {iteration_folder}")
+        current_model = f"{iteration_folder}/{pdb_basename}_{iteration}_{n}.pdb"
+        mpnn_out["model_path"].append(current_model)
+        mpnn_out["input_pdb"].append(pdb)
 
-   iteration_data = [[mpnn_out[k][n] for k in labels] for n in range(initial_proteinmpnns)]
-   initial_dataframe=pd.DataFrame(iteration_data, columns=labels)
+        af_model.save_current_pdb(f"{current_model}")
+        af_model._save_results(save_best=True, verbose=False)
+        af_model._k += 1
 
-   if sort_by=="plddt":
-      initial_dataframe = initial_dataframe.sort_values("rmsd", ascending=False).reset_index(drop=True)
-   else:
-      initial_dataframe = initial_dataframe.sort_values(by="rmsd").reset_index(drop=True)
+    iteration_data = [[mpnn_out[k][n] for k in labels] for n in range(initial_proteinmpnns)]
+    initial_dataframe = pd.DataFrame(iteration_data, columns=labels)
 
-   save_to_csv(initial_dataframe,initial_csv)
+    if sort_by == "plddt":
+        initial_dataframe = initial_dataframe.sort_values("rmsd", ascending=False).reset_index(drop=True)
+    else:
+        initial_dataframe = initial_dataframe.sort_values(by="rmsd").reset_index(drop=True)
+
+    save_to_csv(initial_dataframe, initial_csv)
 else:
     print("Initial dataframe (iteration0) already exists, loading dataframe")
     initial_dataframe = pd.read_csv(initial_csv)
 
 
+def mpnn_seqs(
+    af_model,
+    pdb,
+    target_chains,
+    binder_chains,
+    num_seqs,
+    rm_aa="C",
+    sampling_temp=sampling_temp,
+    batch=mpnn_batch,
+):
+    af_model.prep_inputs(
+        pdb,
+        target_chain=",".join(target_chains),
+        binder_chain=",".join(binder_chains),
+        rm_aa=rm_aa,
+    )
+    mpnn_model.get_af_inputs(af_model)
+    mpnn_out = mpnn_model.sample(num=num_seqs // batch, batch=batch, temperature=sampling_temp)
+    for k in af_terms:
+        mpnn_out[k] = []
+    for term in other_terms:
+        if term not in mpnn_out:
+            mpnn_out[term] = []
 
-def mpnn_seqs(af_model, pdb, target_chains, binder_chains, num_seqs, rm_aa="C", sampling_temp=sampling_temp, batch=mpnn_batch):
-  af_model.prep_inputs(pdb,target_chain=",".join(target_chains),binder_chain=",".join(binder_chains),rm_aa=rm_aa)
-  mpnn_model.get_af_inputs(af_model)
-  mpnn_out = mpnn_model.sample(num=num_seqs//batch, batch=batch, temperature=sampling_temp)
-  for k in af_terms: mpnn_out[k] = []
-  for term in other_terms:
-      if term not in mpnn_out:
-         mpnn_out[term] = []
+    return mpnn_out
 
-  return mpnn_out
-                    
+
 def af2_predict(af_model, pdb, mpnn_out, output_folder, prefix="", num_models=1):
-  
-  num_seq=len(mpnn_out["seq"])
-  os.makedirs(output_folder, exist_ok=True)
 
-  for n in range(num_seq):
-      seq = mpnn_out["seq"][n][-af_model._len:]
-      af_model.predict(seq=seq, num_recycles=num_recycles, num_models=num_models, verbose=False)
-      
-      for t in af_terms: mpnn_out[t].append(af_model.aux["log"][t])
-      if "i_pae" in mpnn_out:
-         mpnn_out["i_pae"][-1] = mpnn_out["i_pae"][-1] * 31
-      if "pae" in mpnn_out:
-         mpnn_out["pae"][-1] = mpnn_out["pae"][-1] * 31
-      
-      current_model=f"{output_folder}/{pdb_basename}_{prefix}_{n}.pdb"
-      mpnn_out["model_path"].append(current_model)
-      mpnn_out["input_pdb"].append(pdb)
+    num_seq = len(mpnn_out["seq"])
+    os.makedirs(output_folder, exist_ok=True)
 
-      af_model.save_current_pdb(f"{current_model}")
-      af_model._save_results(save_best=True, verbose=False)
-      af_model._k += 1
-  
-  # Prepare data after prediction
-  labels = ["score"] + af_terms + other_terms
-  iteration_data = [[mpnn_out[k][n] for k in labels] for n in range(num_seq)]
+    for n in range(num_seq):
+        seq = mpnn_out["seq"][n][-af_model._len :]
+        af_model.predict(seq=seq, num_recycles=num_recycles, num_models=num_models, verbose=False)
 
-  return iteration_data           
+        for t in af_terms:
+            mpnn_out[t].append(af_model.aux["log"][t])
+        if "i_pae" in mpnn_out:
+            mpnn_out["i_pae"][-1] = mpnn_out["i_pae"][-1] * 31
+        if "pae" in mpnn_out:
+            mpnn_out["pae"][-1] = mpnn_out["pae"][-1] * 31
 
-iteration_data=initial_dataframe.head(designs_per_iteration).reset_index(drop=True)
+        current_model = f"{output_folder}/{pdb_basename}_{prefix}_{n}.pdb"
+        mpnn_out["model_path"].append(current_model)
+        mpnn_out["input_pdb"].append(pdb)
+
+        af_model.save_current_pdb(f"{current_model}")
+        af_model._save_results(save_best=True, verbose=False)
+        af_model._k += 1
+
+    # Prepare data after prediction
+    labels = ["score"] + af_terms + other_terms
+    iteration_data = [[mpnn_out[k][n] for k in labels] for n in range(num_seq)]
+
+    return iteration_data
+
+
+iteration_data = initial_dataframe.head(designs_per_iteration).reset_index(drop=True)
 
 # Start iterations
-for i in range(1,iterations+1):
-   print(f"starting iteration {i}")
-   t_it=time.time()
-   
-   # Setup iteration folder
-   iteration_folder=f"{pdb_output}/iterations/it_{i}"
-   os.makedirs(iteration_folder, exist_ok=True)
+for i in range(1, iterations + 1):
+    print(f"starting iteration {i}")
+    t_it = time.time()
 
-   # Check if the output CSV file for the current iteration exists
-   output_csv_path = f"{pdb_output}/it_{i}.csv"
+    # Setup iteration folder
+    iteration_folder = f"{pdb_output}/iterations/it_{i}"
+    os.makedirs(iteration_folder, exist_ok=True)
 
-   if os.path.isfile(output_csv_path):
-      iteration_data=pd.read_csv(output_csv_path)
-      print(f"Skipping iteration {i} as output file already exists.")
-      continue
+    # Check if the output CSV file for the current iteration exists
+    output_csv_path = f"{pdb_output}/it_{i}.csv"
 
-   # Run MPNNs/AF2s on X number of input designs
-   iteration_data = iteration_data.head(designs_per_iteration).reset_index(drop=True)
-   best_10=iteration_data
-   for z,row in best_10.iterrows():
+    if os.path.isfile(output_csv_path):
+        iteration_data = pd.read_csv(output_csv_path)
+        print(f"Skipping iteration {i} as output file already exists.")
+        continue
 
-      mpnn_out=mpnn_seqs(af_model, row["model_path"],"A","B",proteinmpnn_per_input)
-      row_data = af2_predict(af_model, row["model_path"], mpnn_out, iteration_folder, prefix=f"{i}_{z}")
-      row_dataframe = pd.DataFrame(row_data, columns=labels)
+    # Run MPNNs/AF2s on X number of input designs
+    iteration_data = iteration_data.head(designs_per_iteration).reset_index(drop=True)
+    best_10 = iteration_data
+    for z, row in best_10.iterrows():
 
-      # Add row data to iteration data
-      iteration_data = pd.concat([iteration_data, row_dataframe])
-   
-   if sort_by=="plddt":
-      iteration_data = iteration_data.sort_values(sort_by, ascending=False).reset_index(drop=True)
-   else:
-      iteration_data = iteration_data.sort_values(sort_by).reset_index(drop=True)
-   # Save data
-   save_to_csv(iteration_data,output_csv_path) # to iteration file
-   save_to_csv(iteration_data,f"{pdb_output}/all.csv") # to combined file
-   save_to_csv(iteration_data,f"{output_folder}/all.csv") # to all binders
+        mpnn_out = mpnn_seqs(af_model, row["model_path"], "A", "B", proteinmpnn_per_input)
+        row_data = af2_predict(af_model, row["model_path"], mpnn_out, iteration_folder, prefix=f"{i}_{z}")
+        row_dataframe = pd.DataFrame(row_data, columns=labels)
 
-   # Plot progress
-   columns = ['plddt','i_pae']
-   try:
-      #iterations, iteration_folder, columns=None, save_path=None
-      df_plot=create_dataframe(iterations,pdb_output)
-      plot_columns(df_plot, columns, save_path=f"{pdb_output}/iteration_plot.png")
-   except:
-      print("could not plot")
-   print(f"finished iteration in {time.time()-t_it} seconds")
+        # Add row data to iteration data
+        iteration_data = pd.concat([iteration_data, row_dataframe])
+
+    if sort_by == "plddt":
+        iteration_data = iteration_data.sort_values(sort_by, ascending=False).reset_index(drop=True)
+    else:
+        iteration_data = iteration_data.sort_values(sort_by).reset_index(drop=True)
+    # Save data
+    save_to_csv(iteration_data, output_csv_path)  # to iteration file
+    save_to_csv(iteration_data, f"{pdb_output}/all.csv")  # to combined file
+    save_to_csv(iteration_data, f"{output_folder}/all.csv")  # to all binders
+
+    # Plot progress
+    columns = ["plddt", "i_pae"]
+    try:
+        # iterations, iteration_folder, columns=None, save_path=None
+        df_plot = create_dataframe(iterations, pdb_output)
+        plot_columns(df_plot, columns, save_path=f"{pdb_output}/iteration_plot.png")
+    except:
+        print("could not plot")
+    print(f"finished iteration in {time.time()-t_it} seconds")
 
 print(f"done iterating in {time.time()-t0} seconds")
